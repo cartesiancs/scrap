0d1971c644637d548bbc7c12dde7d16927b5a7dd
Dipokal HHJ <hhj@devent.kr>
2023-08-05 13:35:13 +0900

    feat: add select

--- client/src/components/FeedWrite.tsx
+++ client/src/components/FeedWrite.tsx
@@ -6,7 +6,7 @@ import { FeedInput } from './Feed'
 
 import Navbar from './Navbar'
 import CheckSignin from './CheckSignin'
-import { Box, Button } from "@mui/material";
+import { Box, Button, Drawer, Typography } from "@mui/material";
 import { Link } from "react-router-dom";
 
 import Tesseract from 'tesseract.js';
@@ -14,35 +14,80 @@ import Tesseract from 'tesseract.js';
 
 
 
+type Anchor = 'top' | 'left' | 'bottom' | 'right';
 
 function FeedWrite() {
 
 
     const [recognizeText, setRecognizeText] = useState('')
 
+    const [listState, setListState] = React.useState({
+        bottom: false
+    });
+    
+    const toggleDrawer =
+    (anchor: Anchor, open: boolean) =>
+    (event: React.KeyboardEvent | React.MouseEvent) => {
+        if (
+        event.type === 'keydown' &&
+        ((event as React.KeyboardEvent).key === 'Tab' ||
+            (event as React.KeyboardEvent).key === 'Shift')
+        ) {
+        return;
+        }
+
+        setListState({ ...listState, [anchor]: open });
+    };
+    
+
+    const list = (anchor: Anchor) => (
+        <Box
+          sx={{ width: anchor === 'top' || anchor === 'bottom' ? 'auto' : 250 }}
+          role="presentation"
+          onClick={toggleDrawer(anchor, false)}
+          onKeyDown={toggleDrawer(anchor, false)}
+        >
+
+            <TakePicture setRecognizeText={setRecognizeText}></TakePicture>
+            <ListButton>수동으로 입력</ListButton>
+
+        </Box>
+    );
+
+      
+    useEffect(() => {
+        setListState({ ...listState, bottom: true });
+    }, [])
 
     return (
         <CheckSignin>
-            <Box sx={{ marginTop: '5rem' }}>
-                <Navbar>
-                    <Box sx={{ justifyContent: 'center' }}>
-                        <Link to={'/write'}>
-                            <Button variant="text" disableElevation>작성</Button>
-                        </Link>
+            <Navbar>
+                <Box sx={{ justifyContent: 'center' }}>
+                    <Link to={'/write'}>
+                        <Button variant="text" disableElevation>작성</Button>
+                    </Link>
 
-                        <Link to={'/profile'}>
-                            <Button variant="text" disableElevation>프로필</Button>
-                        </Link>
-                    </Box>
-                </Navbar>
+                    <Link to={'/profile'}>
+                        <Button variant="text" disableElevation>프로필</Button>
+                    </Link>
+                </Box>
+            </Navbar>
 
+            <Box sx={{ marginTop: '5rem', display: listState.bottom ? 'none' : 'block' }}>
                 <FeedInput defaultQuotationText={recognizeText}></FeedInput>
-
-                <TakePicture setRecognizeText={setRecognizeText}></TakePicture>
-
-                
             </Box>
 
+            {(['bottom'] as const).map((anchor) => (
+                <React.Fragment key={anchor}>
+                    <Drawer
+                    anchor={anchor}
+                    open={listState[anchor]}
+                    onClose={toggleDrawer(anchor, false)}
+                    >
+                    {list(anchor)}
+                    </Drawer>
+                </React.Fragment>
+            ))}
         </CheckSignin>
 
     );
@@ -81,7 +126,7 @@ function TakePicture({ setRecognizeText }) {
 
     return (
         <Box>
-            <Button onClick={handleClickButton}>이미지 가져오기</Button>
+            <ListButton onClick={handleClickButton}>이미지 가져오기</ListButton>
             <ProcessImageRecognize setRecognizeText={setRecognizeText} isProcess={isProcess} url={imageBlobUrl}></ProcessImageRecognize>
         </Box>
     )
@@ -110,7 +155,20 @@ function ProcessImageRecognize({ setRecognizeText, isProcess, url }) {
     }, [isProcess])
 
     return (
-        <h1>{progress}%</h1>
+        <Typography sx={{ display: 'none' }}>{progress}%</Typography>
+    )
+}
+
+type ListButtonType = {
+    children: any
+    onClick?: any
+}
+
+function ListButton({ children, onClick }: ListButtonType) {
+
+    return (
+        <Button sx={{ padding: '1rem', width: "100%", fontSize: "1.2rem" }} onClick={onClick}>{children}</Button>
+
     )
 }
 
