ec8c9a3349b2cfe93fb34a3541b90a85fffad963
Huh Hyeongjun <48173908+DipokalLab@users.noreply.github.com>
2024-10-10 09:50:23 +0900

    fix: remove google auth

--- server/config/oauth.ts
+++ server/config/oauth.ts
@@ -1,8 +1,8 @@
 export default 
 {
     "GOOGLE": {
-        "GOOGLE_CLIENT_ID": "96629021119-lqj8vp6ugb4ttda0jpt90o3b6o2t2hca.apps.googleusercontent.com",
-        "GOOGLE_CLIENT_SECRET": "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",
+        "GOOGLE_CLIENT_ID": "<PRI>",
+        "GOOGLE_CLIENT_SECRET": "<PRI>",
         "IS_ENABLE": true
     }
 }
